59e86022b6c6af6a6c1b14f051892abf5d0f11e0
hermetaniosheena-cmyk <hermetaniosheena@gmail.com>
2025-11-17 08:30:48 +0800

    Add files via upload

---
+++ cs.js
@@ -0,0 +1,28 @@
+function legalAge(age){
+    if (age >= 18){
+        console.log("you are an adult");
+    } else{
+        console.log("you are not adult");
+    }
+}
+
+legalAge(50);
+
+function computerGrade(grade){
+    if (grade >= 95){
+        console.log("1");
+    } else if (grade >=91.5) {
+        console.log("1.25");
+    } else if (grade >=89.5){
+        console.log("1.5");
+    } else if ("86.5") {
+        console.log("1.75");
+    } else if (grade >= 84.5) {
+        console.log("2");
+
+    } else {
+        console.log("Not within the transmutation");
+    }
+
+    }
+
