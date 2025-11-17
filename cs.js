af23ff979ef668befe251a2cd813710760217b5f
hermetaniosheena-cmyk <hermetaniosheena@gmail.com>
2025-11-17 08:51:42 +0800

    Add files via upload

--- cs.js
+++ cs.js
@@ -1,28 +1,13 @@
-function legalAge(age){
-    if (age >= 18){
-        console.log("you are an adult");
-    } else{
-        console.log("you are not adult");
-    }
-}
 
-legalAge(50);
+    function login(username,password);
+    if (username === "sheena"){
+        console.log("user recognized");
+        if (paassword ===1234){
+            console.log("welcome user");
+        } else {
+            console.log("invalid login credential")
+        }
 
-function computerGrade(grade){
-    if (grade >= 95){
-        console.log("1");
-    } else if (grade >=91.5) {
-        console.log("1.25");
-    } else if (grade >=89.5){
-        console.log("1.5");
-    } else if ("86.5") {
-        console.log("1.75");
-    } else if (grade >= 84.5) {
-        console.log("2");
-
-    } else {
-        console.log("Not within the transmutation");
-    }
-
-    }
-
+       } esle {
+        console.log("invalid user")
+      }
